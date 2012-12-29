e43909de2c33a9088fe8686aa4c0c29d11aa048b
Robbie Clutton <robert.clutton@gmail.com>
2012-12-29 18:48:44 -0500

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pubclick::Application.config.secret_token = 'd0b59bad7ecb417cf23420221255f46637f10ccf4ed4f2c3c1418a26986ebf07e5c8b3d62aa454e0110f0179f6967da8bbbe13fd52b893b356f80eb689d57ae6'
